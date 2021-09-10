63d61a413e96cef71c9dcff2674f45b1680dde83
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-09-11 02:10:14 +0900

    削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # admin controller
   get "/admin/post/new" => "admin#new"
   post "/admin/post/create" => "admin#create"
+  post "/admin/post/:id/delete" => "admin#delete"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
