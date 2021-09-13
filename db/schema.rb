0e195acf8b53f0154f584b9c5add0e0d833121d5
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-09-13 22:51:08 +0900

    タグmodelの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_145805) do
+ActiveRecord::Schema.define(version: 2021_09_13_134948) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "tag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
