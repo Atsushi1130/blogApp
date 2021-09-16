54e1298ecfa638010aa22a002fed30c3e7e0b62b
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-09-17 01:12:13 +0900

    デザインのレスポンシブ化

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all.order(updated_at: "desc")
     @user = User.find_by(id: 1)
+    @get_tags = Tag.all
   end
 
   def detail
