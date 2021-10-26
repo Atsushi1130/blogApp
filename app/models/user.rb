8763b19ad294e068a50e4e6bec5aa7a2a558b5ed
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-10-26 20:40:48 +0900

    imageのpresenceのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	mount_uploader :image,ImageUploader
 	validates :content, {presence: true, length: {maximum: 70}}
-	validates :image, {presence: true}
+	# validates :image, {presence: true}
 	validates :name, {presence: true}
 	validates :email, {presence: true}
 	validates :password, {presence: true}
