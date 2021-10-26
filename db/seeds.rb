1ee4eb3e32b7c9a20f43437690c10790c78b0328
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-10-26 21:21:54 +0900

    rake seedの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(:name => 'あつし', :email => 'atsu.1130.2b@gmail.com', :password => 'semoroti5566', :content => '初期値', :image => '\app\assets\images\default.png')
