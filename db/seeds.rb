cc77bffefd3cf2fe1bd64d5019aa9c06a1162662
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-11-05 20:29:45 +0900

    seeds.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   下のコメントアウトをはずして、自分のすきな値を入力してください。
+
+#   User.create(:name => '名前', :email => 'ログイン時のメールアドレス', :password => 'ログイン時のパスワード', :content => '自己紹介文', :image => File.open('./app/assets/images/default.jpg'))
