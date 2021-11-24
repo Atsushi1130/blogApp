c64d10671b7a10ccb8c5938443376924d7b5e68a
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-11-24 16:29:46 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#   下のコメントアウトをはずして、自分のすきな値を入力してください。
+#   下記を自分のすきな値を入力してください。
 
   User.create(:name => '名前', :email => ENV['S3_email'], :password => ENV['S3_password'], :content => '自己紹介文', :image => File.open('./app/assets/images/default.png'))
