a5141bf5761a3488d72fe9e6b7e8c4e8bb086063
Atsushi1130 <atsu.1130.2b@gmail.com>
2021-12-15 21:44:45 +0900

    rspecの導入

--- config/application.rb
+++ config/application.rb
@@ -21,3 +21,11 @@ module Myblog
     config.time_zone = 'Tokyo'
   end
 end
+
+config.generators do |g|
+  g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      controller_specs: false,
+      routing_specs: false
+end
